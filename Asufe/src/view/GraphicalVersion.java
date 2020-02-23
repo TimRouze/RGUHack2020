38ef11f126fc1eff4eadd58b0b39a523c84cb82c
BenjaminPcrd <benjamin.picard0@gmail.com>
2020-02-23 02:41:13 +0000

    screens

--- Asufe/src/view/GraphicalVersion.java
+++ Asufe/src/view/GraphicalVersion.java
@@ -4,20 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GraphicalVersion extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("TitleScreen.fxml"));
         primaryStage.setTitle("A series of (un)fortunate events");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void handleButton(MouseEvent mouseEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("IntroductiveScreen.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 1280, 720));
+        stage.show();
     }
 }
